0698b66baeeed0f432338caf7f40c74e38bd3956
Daaron Dwyer <daarond@gmail.com>
2017-12-26 17:10:34 -0600

    create channel, first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clinicoin-lambda-functions
+
+Functions developed for AWS Lambda in AWS Cloud9, 
+having higher priviledges than a normal user.
---
+++ createChannel/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+console.log('Loading function');
+
+const aws = require('aws-sdk');
+
+const sqs = new aws.SQS({apiVersion: '2012-11-05'});
+
+var s3 = new aws.S3({apiVersion: '2006-03-01'});
+
+exports.handler = (event, context, callback) => {
+    
+    if (event.queueName == "" || event.queueName == undefined || event.queueName == null) {
+        const response = {
+          "statusCode": 400,
+          "headers": {
+              "requestid": context.awsRequestId,
+          },
+          "body": "Queue create failed: invalid queue name",
+          "isBase64Encoded": false
+        };
+        callback(null, response);
+        return;
+    }
+    
+    console.log("clientID = " + JSON.stringify(context.identity));
+    
+    event.queueName = "Clinicoin-"+event.queueName;
+    
+
+    console.log("Create queue: "+event.queueName);
+    const queue_params = {
+      QueueName: event.queueName+'.fifo',
+      Attributes: {
+        'FifoQueue': 'true'
+      }
+    };
+    const queue_promise = sqs.createQueue(queue_params).promise();
+
+
+    console.log("Create s3 bucket: "+event.queueName);
+    var s3_params = {
+        Bucket: event.queueName
+    };
+    const s3_promise = s3.createBucket(s3_params).promise();
+    
+    Promise.all([
+        s3_promise,
+        queue_promise
+    ]).then(() => {
+        const response = {
+            "statusCode": 200,
+            "headers": {
+                "requestid": context.awsRequestId,
+            },
+            "body": "Channel created "+event.queueName,
+            "isBase64Encoded": false
+        };
+        callback(null, response);
+    });
+};
